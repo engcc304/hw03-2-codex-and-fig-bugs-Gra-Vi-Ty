466968abfd91c071857db57daa1902865213bb4b
Gra-Vi-Ty <worakan223@gmail.com>
2023-08-10 12:55:37 +0700

    upppp

--- Homework/HW03/hw03-2-codex-and-fix-bug.cpp
+++ Homework/HW03/hw03-2-codex-and-fix-bug.cpp
@@ -8,14 +8,17 @@ int main(){
     int i = 0 ;
     if(_1!= 0) {
         printf( "ok" ) ;
-    }else while(i <= 5) {
-        printf( "not-okey-%d  ", ++i ) ;
-    }
-    for(i = 3; i  <10; i += 1) {
-        if( i % 2 == ( !1 ?1 : 2)) {
-            printf("see see") ; 
-            printf("haha") ;
+    } else {
+        while(i <= 5) {
+            printf( "not-okey-%d  ", ++i ) ;
+        }
+        for(i = 3; i < 10; i += 1) {
+            if( i % 2 == ( !1?1 : 2)) {
+                printf("see see") ; 
+                printf("haha") ;
+            }
         }
     }
+    printf("haha") ;
     return 0 ;
 }
